6c3286a96dda45b2425d0d59efbbf6634839601b
Vladyslava Korovaina <vladyslava.korovainaya@gmail.com>
2025-01-12 20:13:08 +0000

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,23 @@
-export const PostInfo = () => <>Put the post here</>;
+// eslint-disable-next-line no-unused-vars
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+      <p>
+        Posted by{' '}
+        <a href={`mailto:${post.user.email}`} className="UserInfo">
+          {post.user.name}
+        </a>
+      </p>
+    </div>
+    <p className="PostInfo__body">{post.body}</p>
+    {post.comments.length > 0 ? (
+      <CommentList comments={post.comments} />
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
+);
